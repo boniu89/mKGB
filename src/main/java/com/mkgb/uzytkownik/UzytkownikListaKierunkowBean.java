385faa123f5d3e7e1ef87e3d69f15cce05d88601
boniu89 <ackbar20@gmail.com>
2012-11-20 16:56:27 +0100

    Commit

---
+++ src/main/java/com/mkgb/uzytkownik/UzytkownikListaKierunkowBean.java
@@ -0,0 +1,11 @@
+package com.mkgb.uzytkownik;
+
+import java.io.Serializable;
+
+public class UzytkownikListaKierunkowBean implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	
+
+}

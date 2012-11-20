e93d1340851cf840e5f55a2b0aac707d59997c82
Beata <chust@wp.pl>
2012-11-20 11:17:17 +0100

    Interfejs graficzny cd.

--- src/main/java/com/mkgb/uzytkownik/UzytkownikLogowanieBean.java
+++ src/main/java/com/mkgb/uzytkownik/UzytkownikLogowanieBean.java
@@ -1,5 +1,6 @@
 package com.mkgb.uzytkownik;
 
+import com.mkgb.uzytkownik.model.Sluchacz;
 import java.io.Serializable;
 
 import com.mkgb.uzytkownik.model.Uzytkownik;
@@ -10,7 +11,7 @@ public class UzytkownikLogowanieBean implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
 	/*Deklaracja zmiennych*/
-    private Uzytkownik user;
+    private Sluchacz user;
     private Boolean loginCorrect;
     private String login;
     private String password;
@@ -20,6 +21,7 @@ public class UzytkownikLogowanieBean implements Serializable{
         this.loginCorrect = false;
         this.login = "";
         this.password = "";
+        this.user = new Sluchacz();
     }
     
     /*Getters and setters*/
@@ -47,10 +49,32 @@ public class UzytkownikLogowanieBean implements Serializable{
     public void setLoginCorrect(Boolean loginCorrect) {
         this.loginCorrect = loginCorrect;
     }
+
+    public Sluchacz getUser() {
+        return user;
+    }
+
+    public void setUser(Sluchacz user) {
+        this.user = user;
+    }
     
     /*Metody*/
     public String login()
     {
+                /*Date do testów*/
+        user.setId(1);
+        user.setImie("Beata");
+        user.setDrugieImie("");
+        user.setNazwisko("Kobyliñska");
+        user.setImieMatki("Danuta");
+        user.setImieOjca("Zbigniew");
+        user.setPesel("89040209080");
+        user.setTelefon("725908787");
+        user.setEmail(login);
+        user.setAdresKorespondencyjny("adres");
+        user.setAdresZamieszkania("adres");
+        user.setHaslo(password);
+        /*-------------*/
         setLoginCorrect(true);
         return "login";
     }
